9c8ff95477eac9a9af7a16dbdeed8eac2002b696
Alon Fluman <alon.fluman@gmail.com>
2023-01-10 15:30:46 +0200

    radio communication

--- main.ts
+++ main.ts
@@ -1,60 +1,11 @@
-let rnd = 0
-input.onGesture(Gesture.Shake, function () {
-    rnd = randint(1, 6)
-    for (let index = 0; index <= rnd - 1; index++) {
-        music.playTone(262, music.beat(BeatFraction.Whole))
-        music.rest(music.beat(BeatFraction.Quarter))
-    }
-    if (rnd == 1) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
-    } else if (rnd == 2) {
-        basic.showLeds(`
-            . . . . .
-            . # . . .
-            . . . . .
-            . . . # .
-            . . . . .
-            `)
-    } else if (rnd == 3) {
-        basic.showLeds(`
-            # . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . #
-            `)
-    } else if (rnd == 4) {
-        basic.showLeds(`
-            # . . . #
-            . . . . .
-            . . . . .
-            . . . . .
-            # . . . #
-            `)
-    } else if (rnd == 5) {
-        basic.showLeds(`
-            # . . . #
-            . . . . .
-            . . # . .
-            . . . . .
-            # . . . #
-            `)
-    } else {
-        basic.showLeds(`
-            # . . . #
-            . . . . .
-            # . . . #
-            . . . . .
-            # . . . #
-            `)
+radio.onReceivedString(function (receivedString) {
+    radio_in = receivedString
+    if (receivedString == "t") {
+        radio.sendNumber(input.temperature())
     }
 })
+let radio_in = ""
+radio.setGroup(88)
 basic.forever(function () {
-	
+    basic.showString(radio_in)
 })
